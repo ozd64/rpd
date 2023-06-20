8ebfe59923d076b5653263033c2e7e900cb023de
Cem Özden <ozden.cem@outlook.com>
2023-06-20 11:21:16 +0200

    Adding infinite loop and CLI operator

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ enum OperationType {
     ADDITION,
     SUBTRACTION,
     MULTIPLICATION,
-    DIVISION
+    DIVISION,
 }
 
 impl Display for OperationType {
@@ -17,7 +17,7 @@ impl Display for OperationType {
             OperationType::ADDITION => write!(f, "+"),
             OperationType::SUBTRACTION => write!(f, "-"),
             OperationType::MULTIPLICATION => write!(f, "*"),
-            OperationType::DIVISION => write!(f, "/")
+            OperationType::DIVISION => write!(f, "/"),
         }
     }
 }
@@ -25,7 +25,7 @@ impl Display for OperationType {
 enum PolishNotationToken {
     Operation(OperationType),
     Number(u32),
-    Space
+    Space,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -36,7 +36,9 @@ enum TokenError {
 impl Display for TokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            TokenError::InvalidCharacter(index, ch) => write!(f, "Invalid character at position {}, \"{}\"", index + 1, ch),
+            TokenError::InvalidCharacter(index, ch) => {
+                write!(f, "Invalid character at position {}, \"{}\"", index + 1, ch)
+            }
         }
     }
 }
@@ -45,57 +47,77 @@ impl Error for TokenError {}
 
 #[derive(Debug, PartialEq, Eq)]
 enum CalculationError {
-   NoNumberFoundForOperation(usize, OperationType),
-   NoResultAvailable(&'static str),
-   IncompleteExpression(usize)
+    NoNumberFoundForOperation(usize, OperationType),
+    NoResultAvailable(&'static str),
+    IncompleteExpression(usize),
 }
 
 impl Display for CalculationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CalculationError::NoNumberFoundForOperation(pos, op) => write!(f, "No number found before the operation {} at position {}", op, pos + 1),
+            CalculationError::NoNumberFoundForOperation(pos, op) => write!(
+                f,
+                "No number found before the operation {} at position {}",
+                op,
+                pos + 1
+            ),
             CalculationError::NoResultAvailable(error_msg) => write!(f, "{}", error_msg),
-            CalculationError::IncompleteExpression(stack_size) => write!(f, "Incomplete expression. {} tokens unprocessed.", stack_size - 1)
+            CalculationError::IncompleteExpression(stack_size) => write!(
+                f,
+                "Incomplete expression. {} tokens unprocessed.",
+                stack_size - 1
+            ),
         }
     }
 }
 
 impl Error for CalculationError {}
 
-
 fn main() {
-    let mut input = String::new();
+    println!("rpd - Reverse Polish Notation calculator");
+    println!("Type \"exit\" to exit");
 
-    print!("Enter the reversed polish notation: ");
+    loop {
+        let mut input = String::new();
 
-    if let Err(err) = io::stdout().flush() {
-        eprintln!("An error occurred while flushing standard output!\n{}", err);
+        print!("rpd> ");
+
+        if let Err(err) = io::stdout().flush() {
+            eprintln!("An error occurred while flushing standard output!\n{}", err);
+        }
+
+        if let Err(err) = io::stdin().read_line(&mut input) {
+            eprintln!("An error occurred while reading the input!\n{}", err);
+        }
+
+        if input.trim() == "exit" {
+            std::process::exit(0);
+        }
+
+        let token_result: Result<Vec<(usize, PolishNotationToken)>, TokenError> = input
+            .trim()
+            .chars()
+            .enumerate()
+            .map(|(pos, ch)| parse_rpd_token(pos, ch))
+            .collect();
+
+        match token_result {
+            Ok(tokens) => match calculate_rpd(tokens) {
+                Ok(result) => println!("{}", result),
+                Err(err) => eprintln!(
+                    "An error occurred while calculating reversed polish notation. {}",
+                    err
+                ),
+            },
+            Err(token_error) => eprintln!(
+                "An error occurred while evaluating reversed polish notation. {}",
+                token_error
+            ),
+        }
     }
-
-    if let Err(err) = io::stdin().read_line(&mut input) {
-        eprintln!("An error occurred while reading the input!\n{}", err);
-    }
-
-
-    let token_result: Result<Vec<(usize, PolishNotationToken)>, TokenError> = input.trim().chars().enumerate()
-        .map(|(pos, ch)| parse_rpd_token(pos, ch))
-        .collect();
-
-    match token_result {
-        Ok(tokens) => {
-            match calculate_rpd(tokens) {
-                Ok(result) => println!("Result: {}", result),
-                Err(err) => eprintln!("An error occurred while calculating reversed polish notation. {}", err)
-            }
-        },
-        Err(token_error) =>
-            eprintln!("An error occurred while evaluating reversed polish notation. {}", token_error)
-    }
-
 }
 
 fn calculate_rpd(tokens: Vec<(usize, PolishNotationToken)>) -> Result<u32, CalculationError> {
-
     let mut stack = VecDeque::<u32>::new();
 
     for token in tokens {
@@ -103,7 +125,7 @@ fn calculate_rpd(tokens: Vec<(usize, PolishNotationToken)>) -> Result<u32, Calcu
             PolishNotationToken::Operation(op) => apply_op(token.0, op, &mut stack)?,
             PolishNotationToken::Number(num) => stack.push_back(num),
             PolishNotationToken::Space => continue,
-        } 
+        }
     }
 
     if stack.len() > 1 {
@@ -115,29 +137,49 @@ fn calculate_rpd(tokens: Vec<(usize, PolishNotationToken)>) -> Result<u32, Calcu
         .ok_or_else(|| CalculationError::NoResultAvailable("No result can be generated."))
 }
 
-fn apply_op(op_pos: usize, op_type: OperationType, stack: &mut VecDeque<u32>) -> Result<(), CalculationError> {
+fn apply_op(
+    op_pos: usize,
+    op_type: OperationType,
+    stack: &mut VecDeque<u32>,
+) -> Result<(), CalculationError> {
     match op_type {
         OperationType::ADDITION => {
-            let y = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
-            let x = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let y = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let x = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
 
             Ok(stack.push_back(x + y))
-        },
+        }
         OperationType::SUBTRACTION => {
-            let y = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
-            let x = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let y = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let x = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
 
             Ok(stack.push_back(x - y))
-        },
+        }
         OperationType::MULTIPLICATION => {
-            let y = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
-            let x = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let y = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let x = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
 
             Ok(stack.push_back(x * y))
-        },
+        }
         OperationType::DIVISION => {
-            let y = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
-            let x = stack.pop_back().ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let y = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
+            let x = stack
+                .pop_back()
+                .ok_or_else(|| CalculationError::NoNumberFoundForOperation(op_pos, op_type))?;
 
             Ok(stack.push_back(x / y))
         }
@@ -146,15 +188,32 @@ fn apply_op(op_pos: usize, op_type: OperationType, stack: &mut VecDeque<u32>) ->
 
 fn parse_rpd_token(index: usize, ch: char) -> Result<(usize, PolishNotationToken), TokenError> {
     match ch {
-        '+' => Ok((index, PolishNotationToken::Operation(OperationType::ADDITION))),
-        '-' => Ok((index, PolishNotationToken::Operation(OperationType::SUBTRACTION))),
-        '*' => Ok((index, PolishNotationToken::Operation(OperationType::MULTIPLICATION))),
-        'x' => Ok((index, PolishNotationToken::Operation(OperationType::MULTIPLICATION))),
-        'X' => Ok((index, PolishNotationToken::Operation(OperationType::MULTIPLICATION))),
-        '/' => Ok((index, PolishNotationToken::Operation(OperationType::DIVISION))),
-        '0' ..= '9' => Ok((index, PolishNotationToken::Number(ch.to_digit(10).unwrap()))),
+        '+' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::ADDITION),
+        )),
+        '-' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::SUBTRACTION),
+        )),
+        '*' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::MULTIPLICATION),
+        )),
+        'x' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::MULTIPLICATION),
+        )),
+        'X' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::MULTIPLICATION),
+        )),
+        '/' => Ok((
+            index,
+            PolishNotationToken::Operation(OperationType::DIVISION),
+        )),
+        '0'..='9' => Ok((index, PolishNotationToken::Number(ch.to_digit(10).unwrap()))),
         ' ' => Ok((index, PolishNotationToken::Space)),
-        _ => Err(TokenError::InvalidCharacter(index, ch))
-
+        _ => Err(TokenError::InvalidCharacter(index, ch)),
     }
 }
